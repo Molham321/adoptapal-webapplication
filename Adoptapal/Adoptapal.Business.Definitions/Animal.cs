69e0535f8219a30bd28bc3b12dc3d06c08d3db51
Molham Al khodari <alkhodari@drherwig.de>
2023-05-23 09:25:10 +0200

    Update Animal db

---
+++ Adoptapal/Adoptapal.Business.Definitions/Animal.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Adoptapal.Business.Definitions
+{
+    public class Animal
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        public string Birthday { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        public string AnimalCategory { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        public string Color { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        public bool IsMale { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        public float Weight { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        public string ImageFilePath { get; set; }
+
+        public User? User { get; set; }
+
+        // hier später nicht gespeichert? -wo dann?
+        [DefaultValue(false)]
+        public bool MarkedAsFavouriteByUser { get; set; }
+    }
+}
