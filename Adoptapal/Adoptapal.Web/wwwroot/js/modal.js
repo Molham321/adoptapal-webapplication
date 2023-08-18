ac5a96eeb9493c5ec784b118d722998af4c2473f
Molham Al khodari <alkhodari@drherwig.de>
2023-08-18 19:43:25 +0200

    add togglePassword for Register and Setting Pages, cleanUp Code

---
+++ Adoptapal/Adoptapal.Web/wwwroot/js/modal.js
@@ -0,0 +1,15 @@
+﻿
+$("#modalBtn").click(function () {
+    $("#testModal").modal("show");
+})
+
+$('[data-dismiss="modal"]').each(function () {
+    $(this).click(function () {
+        $("#testModal").modal("hide");
+    });
+})
+
+$("#saveButton").click(function () {
+    alert("Saved");
+    $("#testModal").modal("hide");
+});
---
+++ Adoptapal/Adoptapal.Web/wwwroot/js/togglePasswordVisibility.js
@@ -0,0 +1,27 @@
+﻿
+document.addEventListener("DOMContentLoaded", function () {
+    const passwordInput = document.querySelector("#password_Id");
+    const passwordToggle = document.querySelector("#password-toggle");
+
+    const confirmPasswordInput = document.querySelector("#confirmPassword_Id");
+    const confirmPasswordToggle = document.querySelector("#confirmPassword-toggle");
+
+    passwordToggle.addEventListener("click", function () {
+        togglePasswordVisibility(passwordInput, passwordToggle);
+    });
+
+    confirmPasswordToggle.addEventListener("click", function () {
+        togglePasswordVisibility(confirmPasswordInput, confirmPasswordToggle);
+    });
+});
+
+function togglePasswordVisibility(inputField, toggleButton) {
+    if (inputField.type === "password") {
+        inputField.type = "text";
+        toggleButton.innerHTML = '<i class="fas fa-eye-slash"></i>';
+    } else {
+        inputField.type = "password";
+        toggleButton.innerHTML = '<i class="fas fa-eye"></i>';
+    }
+}
+
