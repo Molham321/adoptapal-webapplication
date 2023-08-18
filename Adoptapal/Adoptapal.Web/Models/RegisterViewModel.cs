9a7b38e77ae19fdc94e24b3ad166962023b2625b
Molham Al khodari <alkhodari@drherwig.de>
2023-08-18 20:03:36 +0200

    add Login and RegisterViewModel

---
+++ Adoptapal/Adoptapal.Web/Models/RegisterViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Adoptapal.Web.Models
+{
+    public class RegisterViewModel
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Name is Required")]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2}.", MinimumLength = 3)]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is Required")]
+        [EmailAddress(ErrorMessage = "please enter a valid email address")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is Required")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$",
+        ErrorMessage = "The password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number and one special character.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password is Required")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
